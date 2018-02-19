77b390e35fa8e3d7377c24cc8660ddb2b95c762c
larssusaas <larssusaas@gmail.com>
2018-02-19 11:22:08 +0100

    Moved doxygen comment in dcf.h to dcf.c, Doxygen was reading from .c and .h had the best comments

--- src/dcf.c
+++ src/dcf.c
@@ -63,10 +63,14 @@ inline void start_node(CO_Data* d, UNS8 nodeId){
 }
 
 /**
-** @brief Function to be called from post_SlaveBootup
-**
-** @param d
-** @param nodeId
+* @brief Function to be called from post_SlaveBootup
+* for starting the configuration manager
+*
+* @param *d Pointer on a CAN object data structure
+* @param nodeId Id of the slave node
+* @return 0: configuration manager busy
+*         1: nothing to check, node started
+*         2: dcf check started
 */
 UNS8 check_and_start_node(CO_Data* d, UNS8 nodeId)
 {   
@@ -167,12 +171,12 @@ dcferror:
 }
 
 /**
-**
-**
-** @param d
-** @param nodeId
-**
-** @return
+* @brief Init the consise dcf in CO_Data for nodeId
+*
+* @param *d Pointer on a CAN object data structure
+* @param nodeId Id of the slave node
+* @return 1: dcf check started
+*         0: nothing to do
 */
 UNS8 init_consise_dcf(CO_Data* d,UNS8 nodeId)
 {
